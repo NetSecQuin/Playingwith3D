4931daac990e92f0a99b8d1f53babe5c75d86ef3
NetSecQuin <134654399+NetSecQuin@users.noreply.github.com>
2024-04-23 11:59:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.148.0";
 import openSimplexNoise from 'https://cdn.skypack.dev/open-simplex-noise';
-import { GLTFLoader } from '/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader.js';
 
 
 
