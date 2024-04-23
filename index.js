7a20b926bd3039c7fe36d62f9c80ce51f3df8624
NetSecQuin <134654399+NetSecQuin@users.noreply.github.com>
2024-04-23 12:07:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,123 +1,136 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.148.0";
-import openSimplexNoise from 'https://cdn.skypack.dev/open-simplex-noise';
-import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader.js';
+import {
+  Scene,
+  Color,
+  PerspectiveCamera,
+  WebGLRenderer,
+  DirectionalLight,
+  HemisphereLight,
+  Vector3,
+  Clock,
+  AnimationMixer
+} from "three";
+import OrbitControls from "three-orbitcontrols";
+import GLTFLoader from "three-gltf-loader";
 
+let container;
+let camera;
+let renderer;
+let scene;
+let controls;
 
+const mixers = [];
+const clock = new Clock();
 
+function init() {
+  container = document.querySelector("#scene-container");
 
-//VARIABLES
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-const white = 0xffffff;
-const black = 0x000000;
-const yellow = 0xffc800;
-const red = 0xff0000;
-const purple = 0xa200ff;
-const green = 0x03fc2c;
-const blue = 0x031cfc;
-const cyan = 0x03d3fc;
-const pink = 0xff7dcd;
-const lightYellow = 0xffe97d;
+  // Creating the scene
+  scene = new Scene();
+  scene.background = new Color("skyblue");
 
-//SCENE
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(white);
-scene.fog = new THREE.Fog(blue, 4, 6);
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(gridHelper)
+  createCamera();
+  createLights();
+  loadModels();
+  createControls();
+  createRenderer();
 
-//LIGHT SCENE1
-let lightTopColor = new THREE.Color(yellow);
-let lightBackColor = new THREE.Color(red);
-let rectLightColor = new THREE.Color(purple);
+  renderer.setAnimationLoop(() => {
+    update();
+    render();
+  });
+}
 
-const lightTop = new THREE.PointLight(lightTopColor, 10);
-lightTop.position.set(5, 5, 2);
-lightTop.castShadow = true;
-lightTop.shadow.mapSize.width = lightTop.shadow.mapSize.height = 10000;
-lightTop.penumbra = 0.5;
+function createCamera() {
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+  camera = new PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(-1.5, 1.5, 10);
+}
 
-const lightBack = new THREE.SpotLight(lightBackColor, 2);
-lightBack.position.set(0, -3, -1);
+function createLights() {
+  const mainLight = new DirectionalLight(0xffffff, 5);
+  mainLight.position.set(10, 10, 10);
 
-const rectLight = new THREE.RectAreaLight(rectLightColor, 20, 2, 2);
-rectLight.position.set(1, 1, 1);
-rectLight.lookAt(0, 0, 0);
+  const hemisphereLight = new HemisphereLight(0xddeeff, 0x202020, 5);
+  scene.add(mainLight, hemisphereLight);
+}
 
-scene.add(lightTop, lightBack, rectLight);
+function loadModels() {
+  const loader = new GLTFLoader();
 
-//LIGHT SCENE2
-const targetScene2 = new THREE.Object3D();
-targetScene2.position.set(0, -10, 0);
-scene.add(targetScene2);
+  const onLoad = (result, position) => {
+    const model = result.scene.children[0];
+    model.position.copy(position);
+    model.scale.set(0.05, 0.05, 0.05);
 
-const lightRight = new THREE.SpotLight(pink, 1);
-lightRight.position.set(8, 0, 0);
-lightRight.target = targetScene2;
+    const mixer = new AnimationMixer(model);
+    mixers.push(mixer);
 
-const lightLeft = new THREE.SpotLight(pink, 1);
-lightLeft.position.set(-8, 0, 0);
-lightLeft.target = targetScene2;
+    const animation = result.animations[0];
+    const action = mixer.clipAction(animation);
+    action.play();
 
-const lightMidSpot = new THREE.SpotLight(lightYellow, 2);
-lightMidSpot.position.set(0, -5, 20);
-lightMidSpot.target = targetScene2;
+    scene.add(model);
+  };
 
-const lightMidPoint = new THREE.PointLight(lightYellow, 0.05);
-lightMidPoint.position.set(0, 0, -23);
+  const onProgress = progress => {};
 
-scene.add(lightRight,lightLeft, lightMidSpot, lightMidPoint);
+  const parrotPosition = new Vector3(0, 0, 2.5);
+  loader.load(
+    "/src/models/Parrot.glb",
+    gltf => onLoad(gltf, parrotPosition),
+    onProgress
+  );
 
+  const flamingoPosition = new Vector3(7.5, 0, -10);
+  loader.load(
+    "/src/models/Flamingo.glb",
+    gltf => onLoad(gltf, flamingoPosition),
+    onProgress
+  );
 
-//CAMERA  scene1(-0.3, 0, 5)   scene2(0, -4.5, 10)
-let updateCamPos = new THREE.Vector3(-0.3, 0, 5);
-const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 1, 500);
-camera.position.set(-0.3, 0, 5);
-scene.add(camera);
+  const storkPosition = new Vector3(0, -2.5, -10);
+  loader.load(
+    "/src/models/Stork.glb",
+    gltf => onLoad(gltf, storkPosition),
+    onProgress
+  );
+}
 
-//RENDERER
-const canvas = document.querySelector(".webgl");
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-  antialias: true,
-  alpha: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setClearAlpha(0);
-renderer.shadowMap.enabled = false;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.shadowMap.needsUpdate = true;
+function createRenderer() {
+  renderer = new WebGLRenderer({ antialias: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.gammaFactor = 2.2;
+  renderer.gammaOutput = true;
+  renderer.physicallyCorrectLights = true;
 
-// Instantiate a loader
-const loader = new GLTFLoader();
+  container.appendChild(renderer.domElement);
+}
 
-// Load a glTF model
-loader.load(
-    // URL of the glTF file
-    'scene.gltf',
+function createControls() {
+  controls = new OrbitControls(camera, container);
+}
 
-    // onLoad callback
-    (gltf) => {
-        // The loaded object is a Three.js group containing the model(s)
-        const laptopModel = gltf.scene;
+function update() {
+  const delta = clock.getDelta();
+  mixers.forEach(mixer => mixer.update(delta));
+}
 
-        // Position and scale the model as needed
-        laptopModel.position.set(0, 0, 0);
-        laptopModel.scale.set(0.1, 0.1, 0.1);
+function render() {
+  renderer.render(scene, camera);
+}
 
-        // Add the model to the scene
-        scene.add(laptopModel);
-    },
+init();
 
-    // onProgress callback
-    undefined,
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
 
-    // onError callback
-    (error) => {
-        console.error('Error loading glTF model - the file may not work', error);
-    }
-);
+  // Update camera frustum
+  camera.updateProjectionMatrix();
 
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+window.addEventListener("resize", onWindowResize, false);
